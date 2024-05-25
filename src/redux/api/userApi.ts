0b61b74a94406c374ae2c09c1eae12a5cf1fc634
toushik018 <tayebhossain018@gmail.com>
2024-05-25 19:45:51 +0600

    created the user profile with update info

--- src/redux/api/userApi.ts
+++ src/redux/api/userApi.ts
@@ -1,21 +1,26 @@
-
-import { tagTypes } from "../tagTypes"
-import { baseApi } from "./baseApi"
-
+import { tagTypes } from "../tagTypes";
+import { baseApi } from "./baseApi";
 
 const userApi = baseApi.injectEndpoints({
     endpoints: (build) => ({
-
         getMyProfile: build.query({
             query: () => ({
                 url: "/profile",
-                method: "GET"
+                method: "GET",
             }),
-            providesTags: [tagTypes.user]
-        })
+            providesTags: [tagTypes.user],
+        }),
+        updateProfile: build.mutation({
+            query: (data) => {
+                return {
+                    url: "/profile",
+                    method: "PUT",
+                    data: data,
+                };
+            },
+            invalidatesTags: [tagTypes.user],
+        }),
     }),
-})
+});
 
-export const {
-    useGetMyProfileQuery
-} = userApi
+export const { useGetMyProfileQuery, useUpdateProfileMutation } = userApi;
